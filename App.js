8ba95f3f7e1625ab384e92c0272bc0c47bf4df81
MrMooooYT <mrmooooyt@gmail.com>
2023-05-27 17:53:52 -0400

    w

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Dimensions, Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const width = Dimensions.get("window").width;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{
+        flexDirection: width > 1000 ? "row" : "column"
+      }}>
+        <View style={{margin: 50}}>
+          <Text style={{marginTop: 30, color: "#F4E46E", fontSize: 30}}>cows</Text>
+          <Text style={{marginTop: 30, color: "#FFFFFF", fontSize: 37.5, width: 270}}>Best place to buy & sell crypto</Text>
+          <View style={{flexDirection: "row", borderColor: "#F4E46E", borderWidth: 2, padding: 15, marginTop: 30, borderRadius: 10, width: 200}}>
+            <Text style = {{marginRight: 12.5, color: "#F4E46E", fontSize: 22}}>Get Started</Text>
+            <Image source={require("./assets/stocks.png")} style={{width: 25, height: 25}}/>
+          </View>
+        </View>
+        <Image source={require("./assets/moneystuff.png")} style={{width: 400, height: 400}}/>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
